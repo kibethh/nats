9d4777ace8a07e78d32460bf717e01c5cc87e91e
kibethh <kibethh@gmail.com>
2022-06-16 13:01:23 +0300

    More refactoring on publishing events

--- src/listener.ts
+++ src/listener.ts
@@ -1,5 +1,6 @@
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 import { randomBytes } from "crypto";
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 
 console.clear();
 
@@ -18,28 +19,30 @@ stan.on("connect", () => {
   // setManualAckMode for reverting automatic acknowledgement of an event
   // setDeliverAllAvailable for recovering all events
   // setDurableName for processing unprocessed events
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable() // Getting all events emitted in the past
-    .setDurableName("Ticketing"); //Keeping track of all events gone to a subscription
+  // const options = stan
+  //   .subscriptionOptions()
+  //   .setManualAckMode(true)
+  //   .setDeliverAllAvailable() // Getting all events emitted in the past
+  //   .setDurableName("Ticketing"); //Keeping track of all events gone to a subscription
   // Queue group for preventing if we temporarily disconnect from client,
   // it will not dump the entire durable subscription,
   // Queue group will persist the subscription, for keeping track
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "ordersListenerQueueGroup", //Q-grp for preventing accidental dumping of events
-    options
-  );
-  subscription.on("message", (msg: Message) => {
-    console.log("message received");
+  // const subscription = stan.subscribe(
+  //   "ticket:created",
+  //   "ordersListenerQueueGroup", //Q-grp for preventing accidental dumping of events
+  //   options
+  // );
+  // subscription.on("message", (msg: Message) => {
+  //   console.log("message received");
 
-    const data = msg.getData();
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  //   const data = msg.getData();
+  //   if (typeof data === "string") {
+  //     console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+  //   }
+  //   msg.ack();
+  // });
+
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
