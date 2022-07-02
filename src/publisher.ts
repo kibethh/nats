8dfd3e7ad7af5ab4848a4bef772883aa4b39b291
kibethh <kibethh@gmail.com>
2022-07-02 20:22:43 +0100

    updated publishers

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,3 +1,4 @@
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 import nats, { Message } from "node-nats-streaming";
 
 console.clear();
@@ -5,14 +6,25 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
-  console.log("Publisher connected to NATS");
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
-  stan.publish("ticket:created", data, () => {
-    console.log("Event Published!");
-  });
+stan.on("connect", async () => {
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "abc",
+      title: "concert",
+      price: 20,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  // console.log("Publisher connected to NATS");
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "concert",
+  //   price: 20,
+  // });
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event Published!");
+  // });
 });
